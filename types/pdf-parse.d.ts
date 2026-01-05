58ca76c36d57da5899b3e8d0cb294c3bf27e4ef0
Daniel Raphael <danielraphaelh@gmail.com>
2026-01-05 13:04:17 -0500

    Add TypeScript type definitions for pdf-parse

---
+++ types/pdf-parse.d.ts
@@ -0,0 +1,35 @@
+declare module 'pdf-parse' {
+  interface PDFInfo {
+    PDFFormatVersion?: string;
+    IsAcroFormPresent?: boolean;
+    IsXFAPresent?: boolean;
+    Title?: string;
+    Author?: string;
+    Subject?: string;
+    Keywords?: string;
+    Creator?: string;
+    Producer?: string;
+    CreationDate?: string;
+    ModDate?: string;
+    Trapped?: string;
+  }
+
+  interface PDFMetadata {
+    _metadata?: any;
+    info?: PDFInfo;
+    metadata?: any;
+  }
+
+  interface PDFData {
+    numpages: number;
+    numrender: number;
+    info: PDFInfo;
+    metadata: PDFMetadata | null;
+    text: string;
+    version: string;
+  }
+
+  function pdfParse(dataBuffer: Buffer, options?: any): Promise<PDFData>;
+  export = pdfParse;
+}
+
